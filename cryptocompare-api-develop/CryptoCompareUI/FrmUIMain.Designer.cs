5a16f658b9b35b7a14e478b0ba7ba9090fef8dd6
Frank van Bokhoven <frankvanbokhoven@yggdra.nl>
2018-07-12 11:46:05 +0200

    Persian MaterialSkin

--- cryptocompare-api-develop/CryptoCompareUI/FrmUIMain.Designer.cs
+++ cryptocompare-api-develop/CryptoCompareUI/FrmUIMain.Designer.cs
@@ -28,60 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPageCharts = new System.Windows.Forms.TabPage();
             this.panel5 = new System.Windows.Forms.Panel();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.clbStocks = new System.Windows.Forms.CheckedListBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.btnLoadSelected = new System.Windows.Forms.Button();
-            this.btnClose = new System.Windows.Forms.Button();
-            this.clbStocks = new System.Windows.Forms.CheckedListBox();
-            this.panel6 = new System.Windows.Forms.Panel();
             this.btnYahoo = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnLoadSelected = new System.Windows.Forms.Button();
+            this.tabPageData = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tabPageCharts.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
-            // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1192, 37);
-            this.panel1.TabIndex = 0;
-            // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPageCharts);
+            this.tabControl1.Controls.Add(this.tabPageData);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 37);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1192, 651);
+            this.tabControl1.Size = new System.Drawing.Size(1192, 688);
             this.tabControl1.TabIndex = 1;
             // 
-            // tabPage1
+            // tabPageCharts
             // 
-            this.tabPage1.Controls.Add(this.panel5);
-            this.tabPage1.Controls.Add(this.splitter1);
-            this.tabPage1.Controls.Add(this.panel4);
-            this.tabPage1.Controls.Add(this.panel2);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1184, 625);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            this.tabPageCharts.Controls.Add(this.panel5);
+            this.tabPageCharts.Controls.Add(this.splitter1);
+            this.tabPageCharts.Controls.Add(this.panel4);
+            this.tabPageCharts.Controls.Add(this.panel2);
+            this.tabPageCharts.Location = new System.Drawing.Point(4, 22);
+            this.tabPageCharts.Name = "tabPageCharts";
+            this.tabPageCharts.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageCharts.Size = new System.Drawing.Size(1184, 662);
+            this.tabPageCharts.TabIndex = 0;
+            this.tabPageCharts.Text = "Charts";
+            this.tabPageCharts.UseVisualStyleBackColor = true;
+            this.tabPageCharts.Click += new System.EventHandler(this.tabPage1_Click);
             // 
             // panel5
             // 
@@ -89,14 +80,14 @@
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel5.Location = new System.Drawing.Point(143, 40);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(1038, 582);
+            this.panel5.Size = new System.Drawing.Size(1038, 619);
             this.panel5.TabIndex = 4;
             // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(140, 40);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 582);
+            this.splitter1.Size = new System.Drawing.Size(3, 619);
             this.splitter1.TabIndex = 3;
             this.splitter1.TabStop = false;
             // 
@@ -108,8 +99,26 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel4.Location = new System.Drawing.Point(3, 40);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(137, 582);
+            this.panel4.Size = new System.Drawing.Size(137, 619);
             this.panel4.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel6.Location = new System.Drawing.Point(0, 569);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(137, 50);
+            this.panel6.TabIndex = 2;
+            // 
+            // clbStocks
+            // 
+            this.clbStocks.BackColor = System.Drawing.Color.DarkGray;
+            this.clbStocks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clbStocks.FormattingEnabled = true;
+            this.clbStocks.Location = new System.Drawing.Point(0, 0);
+            this.clbStocks.Name = "clbStocks";
+            this.clbStocks.Size = new System.Drawing.Size(137, 619);
+            this.clbStocks.TabIndex = 1;
             // 
             // panel2
             // 
@@ -131,26 +140,16 @@
             this.panel3.Size = new System.Drawing.Size(1178, 37);
             this.panel3.TabIndex = 1;
             // 
-            // tabPage2
+            // btnYahoo
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1184, 625);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // btnLoadSelected
-            // 
-            this.btnLoadSelected.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnLoadSelected.Location = new System.Drawing.Point(0, 0);
-            this.btnLoadSelected.Name = "btnLoadSelected";
-            this.btnLoadSelected.Size = new System.Drawing.Size(110, 37);
-            this.btnLoadSelected.TabIndex = 1;
-            this.btnLoadSelected.Text = "Load (selected)";
-            this.btnLoadSelected.UseVisualStyleBackColor = true;
-            this.btnLoadSelected.Click += new System.EventHandler(this.btnLoadSelected_Click);
+            this.btnYahoo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnYahoo.Location = new System.Drawing.Point(110, 0);
+            this.btnYahoo.Name = "btnYahoo";
+            this.btnYahoo.Size = new System.Drawing.Size(75, 37);
+            this.btnYahoo.TabIndex = 3;
+            this.btnYahoo.Text = "button1";
+            this.btnYahoo.UseVisualStyleBackColor = true;
+            this.btnYahoo.Click += new System.EventHandler(this.btnYahoo_Click);
             // 
             // btnClose
             // 
@@ -166,34 +165,26 @@
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // clbStocks
+            // btnLoadSelected
             // 
-            this.clbStocks.BackColor = System.Drawing.Color.DarkGray;
-            this.clbStocks.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.clbStocks.FormattingEnabled = true;
-            this.clbStocks.Location = new System.Drawing.Point(0, 0);
-            this.clbStocks.Name = "clbStocks";
-            this.clbStocks.Size = new System.Drawing.Size(137, 582);
-            this.clbStocks.TabIndex = 1;
+            this.btnLoadSelected.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnLoadSelected.Location = new System.Drawing.Point(0, 0);
+            this.btnLoadSelected.Name = "btnLoadSelected";
+            this.btnLoadSelected.Size = new System.Drawing.Size(110, 37);
+            this.btnLoadSelected.TabIndex = 1;
+            this.btnLoadSelected.Text = "Load (selected)";
+            this.btnLoadSelected.UseVisualStyleBackColor = true;
+            this.btnLoadSelected.Click += new System.EventHandler(this.btnLoadSelected_Click);
             // 
-            // panel6
+            // tabPageData
             // 
-            this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel6.Location = new System.Drawing.Point(0, 532);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(137, 50);
-            this.panel6.TabIndex = 2;
-            // 
-            // btnYahoo
-            // 
-            this.btnYahoo.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnYahoo.Location = new System.Drawing.Point(110, 0);
-            this.btnYahoo.Name = "btnYahoo";
-            this.btnYahoo.Size = new System.Drawing.Size(75, 37);
-            this.btnYahoo.TabIndex = 3;
-            this.btnYahoo.Text = "button1";
-            this.btnYahoo.UseVisualStyleBackColor = true;
-            this.btnYahoo.Click += new System.EventHandler(this.btnYahoo_Click);
+            this.tabPageData.Location = new System.Drawing.Point(4, 22);
+            this.tabPageData.Name = "tabPageData";
+            this.tabPageData.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageData.Size = new System.Drawing.Size(1184, 662);
+            this.tabPageData.TabIndex = 1;
+            this.tabPageData.Text = "Data";
+            this.tabPageData.UseVisualStyleBackColor = true;
             // 
             // FrmUIMain
             // 
@@ -201,13 +192,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1192, 688);
             this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.panel1);
             this.Name = "FrmUIMain";
             this.Text = "tootA Crypto UI";
             this.Load += new System.EventHandler(this.FrmUIMain_Load);
             this.Shown += new System.EventHandler(this.FrmUIMain_Shown);
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
+            this.tabPageCharts.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
@@ -216,13 +206,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPageCharts;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPageData;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.Panel panel4;
--- cryptocompare-api-develop/CryptoCompareUI/FrmUIMain.cs
+++ cryptocompare-api-develop/CryptoCompareUI/FrmUIMain.cs
@@ -10,14 +10,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CryptoCompare;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace CryptoCompareUI
 {
-    public partial class FrmUIMain : Form
+    public partial class FrmUIMain : MaterialForm
     {
         public FrmUIMain()
         {
             InitializeComponent();
+            //materialskin. zie: https://github.com/bezzad/PersianMaterialSkin
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.Light;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.White);
+
         }
 
         private void tabPage1_Click(object sender, EventArgs e)
@@ -62,94 +70,94 @@ namespace CryptoCompareUI
 
         private void btnYahoo_Click(object sender, EventArgs e)
         {
-            void DownloadDataFromWeb(string symbol)
-            {
-                DateTime startDate = DateTime.Parse("1900-01-01");
+//            void DownloadDataFromWeb(string symbol)
+//            {
+//                DateTime startDate = DateTime.Parse("1900-01-01");
 
-                string baseURL = "http://ichart.finance.yahoo.com/table.csv?";
-                string queryText = BuildHistoricalDataRequest(symbol, startDate, DateTime.Today);
-                string url = string.Format("{0}{1}", baseURL, queryText);
+//                string baseURL = "http://ichart.finance.yahoo.com/table.csv?";
+//                string queryText = BuildHistoricalDataRequest(symbol, startDate, DateTime.Today);
+//                string url = string.Format("{0}{1}", baseURL, queryText);
 
-                //Get page showing the table with the chosen indices
-                HttpWebRequest request = null;
-                HttpWebResponse response = null;
-                StreamReader stReader = null;
+//                //Get page showing the table with the chosen indices
+//                HttpWebRequest request = null;
+//                HttpWebResponse response = null;
+//                StreamReader stReader = null;
 
-                //csv content
-                string docText = string.Empty;
-                string csvLine = null;
-                try
-                {
-                    request = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
-                    request.Timeout = 300000;
+//                //csv content
+//                string docText = string.Empty;
+//                string csvLine = null;
+//                try
+//                {
+//                    request = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
+//                    request.Timeout = 300000;
 
-                    response = (HttpWebResponse)request.GetResponse();
+//                    response = (HttpWebResponse)request.GetResponse();
 
-                    stReader = new StreamReader(response.GetResponseStream(), true);
+//                    stReader = new StreamReader(response.GetResponseStream(), true);
 
-                    stReader.ReadLine();//skip the first (header row)
-                    while ((csvLine = stReader.ReadLine()) != null)
-                    {
-                        string[] sa = csvLine.Split(new char[] { ',' });
+//                    stReader.ReadLine();//skip the first (header row)
+//                    while ((csvLine = stReader.ReadLine()) != null)
+//                    {
+//                        string[] sa = csvLine.Split(new char[] { ',' });
 
-                        DateTime date = DateTime.Parse(sa[0].Trim('"'));
-                        Double open = double.Parse(sa[1]);
-                        Double high = double.Parse(sa[2]);
-                        Double low = double.Parse(sa[3]);
-                        Double close = double.Parse(sa[4]);
-                        Double volume = double.Parse(sa[5]);
-                        Double adjClose = double.Parse(sa[6]);
-                        // Process the data (e.g. insert into DB)
-                    }
-                }
-                catch (Exception e)
-                {
-                    throw e;
-                }
-            }
+//                        DateTime date = DateTime.Parse(sa[0].Trim('"'));
+//                        Double open = double.Parse(sa[1]);
+//                        Double high = double.Parse(sa[2]);
+//                        Double low = double.Parse(sa[3]);
+//                        Double close = double.Parse(sa[4]);
+//                        Double volume = double.Parse(sa[5]);
+//                        Double adjClose = double.Parse(sa[6]);
+//                        // Process the data (e.g. insert into DB)
+//                    }
+//                }
+//                catch (Exception e)
+//                {
+//                    throw e;
+//                }
+//            }
 
-            string BuildHistoricalDataRequest(string symbol, DateTime startDate, DateTime endDate)
-            {
-                // We're subtracting 1 from the month because yahoo
-                // counts the months from 0 to 11 not from 1 to 12.
-                StringBuilder request = new StringBuilder();
-                request.AppendFormat("s={0}", symbol);
-                request.AppendFormat("&a={0}", startDate.Month - 1);
-                request.AppendFormat("&b={0}", startDate.Day);
-                request.AppendFormat("&c={0}", startDate.Year);
-                request.AppendFormat("&d={0}", endDate.Month - 1);
-                request.AppendFormat("&e={0}", endDate.Day);
-                request.AppendFormat("&f={0}", endDate.Year);
-                request.AppendFormat("&g={0}", "d"); //daily
+//            string BuildHistoricalDataRequest(string symbol, DateTime startDate, DateTime endDate)
+//            {
+//                // We're subtracting 1 from the month because yahoo
+//                // counts the months from 0 to 11 not from 1 to 12.
+//                StringBuilder request = new StringBuilder();
+//                request.AppendFormat("s={0}", symbol);
+//                request.AppendFormat("&a={0}", startDate.Month - 1);
+//                request.AppendFormat("&b={0}", startDate.Day);
+//                request.AppendFormat("&c={0}", startDate.Year);
+//                request.AppendFormat("&d={0}", endDate.Month - 1);
+//                request.AppendFormat("&e={0}", endDate.Day);
+//                request.AppendFormat("&f={0}", endDate.Year);
+//                request.AppendFormat("&g={0}", "d"); //daily
 
-                return request.ToString();
-            }
+//                return request.ToString();
+//            }
 
-        //    The code above will go through each data instance in the CSV file, so you just need to save the data instances to arrays.Calculating the return should be straight forward from then on.
+//        //    The code above will go through each data instance in the CSV file, so you just need to save the data instances to arrays.Calculating the return should be straight forward from then on.
 
-// Create your data lists
-            List<DateTime> date = new List<DateTime>();
-            List<Double> open = new List<Double>();
-            List<Double> high = new List<Double>();
-            List<Double> low = new List<Double>();
-            List<Double> close = new List<Double>();
-            List<Double> volume = new List<Double>();
-            List<Double> adjClose = new List<Double>();
+//// Create your data lists
+//            List<DateTime> date = new List<DateTime>();
+//            List<Double> open = new List<Double>();
+//            List<Double> high = new List<Double>();
+//            List<Double> low = new List<Double>();
+//            List<Double> close = new List<Double>();
+//            List<Double> volume = new List<Double>();
+//            List<Double> adjClose = new List<Double>();
 
-            //
-            // ...
-            //
+//            //
+//            // ...
+//            //
 
-            // inside the DownloadDataFromWeb function:
+//            // inside the DownloadDataFromWeb function:
 
-            // Add the data points as you're going through the loop
-            date.Add(DateTime.Parse(sa[0].Trim('"')));
-            open.Add(double.Parse(sa[1]));
-            high.Add(double.Parse(sa[2]));
-            low.Add(double.Parse(sa[3]));
-            close.Add(double.Parse(sa[4]));
-            volume.Add(double.Parse(sa[5]));
-            adjClose.Add(double.Parse(sa[6]));
+//            // Add the data points as you're going through the loop
+//            date.Add(DateTime.Parse(sa[0].Trim('"')));
+//            open.Add(double.Parse(sa[1]));
+//            high.Add(double.Parse(sa[2]));
+//            low.Add(double.Parse(sa[3]));
+//            close.Add(double.Parse(sa[4]));
+//            volume.Add(double.Parse(sa[5]));
+//            adjClose.Add(double.Parse(sa[6]));
 
             //
             // ...
